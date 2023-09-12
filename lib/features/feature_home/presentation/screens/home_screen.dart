867ee1093f61cab973911389f50ec977a8762168
sajjad <sajjad.flutter@gmail.com>
2023-09-12 11:56:36 +0430

    categories fixed in home screen.

--- lib/features/feature_home/presentation/screens/home_screen.dart
+++ lib/features/feature_home/presentation/screens/home_screen.dart
@@ -3,12 +3,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:price_online/common/bloc/change_index/change_index_cubit.dart';
-import 'package:price_online/common/helper/decimal_rounder.dart';
-import 'package:price_online/features/feature_prices/presentation/bloc/prices_cubit/prices_cubit.dart';
-import 'package:price_online/features/feature_prices/presentation/widgets/lebel_button.dart';
-import 'package:price_online/features/feature_prices/presentation/widgets/price_item.dart';
+import 'package:price_online/features/feature_home/presentation/widgets/category_widget.dart';
 import 'package:price_online/main.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
@@ -60,7 +55,7 @@ class _HomeScreenState extends State<HomeScreen> {
     timer ??= Timer.periodic(
       const Duration(seconds: 4),
       (timer) {
-        if (currentSlide < 4) {
+        if (currentSlide < 3) {
           currentSlide++;
         } else {
           currentSlide = 0;
@@ -74,14 +69,23 @@ class _HomeScreenState extends State<HomeScreen> {
       },
     );
 
-    // // Categories
-    // List<String> categoryImages = [
-    //   'assets/images/gold.svg',
-    //   'assets/images/coin.svg',
-    //   'assets/images/money.svg',
-    //   'assets/images/bitcoin.svg',
-    // ];
-    List<String> categoryTitles = ['طلا', 'سکه', 'ارز مرجع', 'ارز دیجیتال'];
+    // Categories
+    List<String> categoryImages = [
+      'assets/images/gold.svg',
+      'assets/images/coin.svg',
+      'assets/images/money.svg',
+      'assets/images/bitcoin.svg',
+      'assets/images/energy.svg',
+      'assets/images/metals.svg',
+    ];
+    List<String> categoryTitles = [
+      'طلا',
+      'سکه',
+      'ارز مرجع',
+      'ارز دیجیتال',
+      'نفت و انرژی',
+      'فلزات گرانبها',
+    ];
 
     return Scaffold(
       // drawer
@@ -121,281 +125,126 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       body: SafeArea(
-        child: Builder(builder: (context) {
-          // call price api
-          switch (HomeScreen.labelTitle) {
-            case 'طلا':
-              BlocProvider.of<PricesCubit>(context).callGoldDataEvent();
-              break;
-            case 'سکه':
-              BlocProvider.of<PricesCubit>(context).callCoinDataEvent();
-              break;
-            case 'ارز مرجع':
-              BlocProvider.of<PricesCubit>(context).callCurrencyDataEvent();
-              break;
-            case 'ارز دیجیتال':
-              BlocProvider.of<PricesCubit>(context).callCryptoDataEvent();
-              break;
-            default:
-          }
-
-          return Column(
-            children: [
-              // appbar
-              Container(
-                color: Theme.of(context).scaffoldBackgroundColor,
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Builder(
-                          builder: (context) => IconButton(
-                            icon: Icon(
-                              Icons.menu_rounded,
-                              color: Colors.grey.shade800,
-                              size: 24.0,
-                            ),
-                            onPressed: () => Scaffold.of(context).openDrawer(),
-                            tooltip: MaterialLocalizations.of(context)
-                                .openAppDrawerTooltip,
-                          ),
-                        ),
-                        // title appbar
-                        Text('قیمت آنلاین', style: textTheme.titleLarge),
-                        const SizedBox(
-                          width: 45.0,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              // sliders
-              Column(
+        child: Column(
+          children: [
+            // appbar
+            Container(
+              color: Theme.of(context).scaffoldBackgroundColor,
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Column(
                 children: [
-                  SizedBox(
-                    width: width,
-                    height: 160,
-                    child: PageView.builder(
-                      physics: const BouncingScrollPhysics(),
-                      itemCount: sliderImages.length,
-                      controller: pageViewController,
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          onTap: () {
-                            // print(sliderImages[index]);
-                          },
-                          child: Container(
-                            margin: const EdgeInsets.all(10.0),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(25.0),
-                              child: Image(
-                                image: AssetImage(sliderImages[index]),
-                                fit: BoxFit.fill,
-                              ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Builder(
+                        builder: (context) => IconButton(
+                          icon: Icon(
+                            Icons.menu_rounded,
+                            color: Colors.grey.shade800,
+                            size: 24.0,
+                          ),
+                          onPressed: () => Scaffold.of(context).openDrawer(),
+                          tooltip: MaterialLocalizations.of(context)
+                              .openAppDrawerTooltip,
+                        ),
+                      ),
+                      // title appbar
+                      Text('قیمت آنلاین', style: textTheme.titleLarge),
+                      const SizedBox(
+                        width: 45.0,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            // sliders
+            Column(
+              children: [
+                SizedBox(
+                  width: width,
+                  height: 160,
+                  child: PageView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: sliderImages.length,
+                    controller: pageViewController,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          // print(sliderImages[index]);
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.all(10.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(25.0),
+                            child: Image(
+                              image: AssetImage(sliderImages[index]),
+                              fit: BoxFit.fill,
                             ),
                           ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                // page indicator
+                sliderImages.length > 1
+                    ? SmoothPageIndicator(
+                        controller: pageViewController,
+                        count: sliderImages.length,
+                        effect: ExpandingDotsEffect(
+                          dotHeight: 8.0,
+                          dotWidth: 8.0,
+                          activeDotColor: primaryColor,
+                          dotColor: Colors.grey.shade400,
+                          spacing: 5.0,
+                        ),
+                      )
+                    : Container(),
+              ],
+            ),
+            //
+            const SizedBox(height: 10.0),
+            // categories
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 5.0),
+                    child: Text('بازار ها', style: textTheme.titleMedium),
+                  ),
+                  const SizedBox(height: 15.0),
+                  SizedBox(
+                    height: 300,
+                    child: GridView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 15.0,
+                        mainAxisSpacing: 15.0,
+                      ),
+                      itemCount: 6,
+                      itemBuilder: (context, index) {
+                        return CategoryWidget(
+                          image: categoryImages[index],
+                          title: categoryTitles[index],
+                          textTheme: textTheme,
+                          cardColor: cardColor,
+                          primaryColor: primaryColor,
                         );
                       },
                     ),
                   ),
-                  // page indicator
-                  sliderImages.length > 1
-                      ? SmoothPageIndicator(
-                          controller: pageViewController,
-                          count: sliderImages.length,
-                          effect: ExpandingDotsEffect(
-                            dotHeight: 8.0,
-                            dotWidth: 8.0,
-                            activeDotColor: primaryColor,
-                            dotColor: Colors.grey.shade400,
-                            spacing: 5.0,
-                          ),
-                        )
-                      : Container(),
                 ],
               ),
-              //
-              const SizedBox(height: 22.0),
-              // // categories
-              // Padding(
-              //   padding: const EdgeInsets.symmetric(horizontal: 15.0),
-              //   child: Column(
-              //     crossAxisAlignment: CrossAxisAlignment.start,
-              //     children: [
-              //       Padding(
-              //         padding: const EdgeInsets.only(right: 5.0),
-              //         child: Text('بازار ها', style: textTheme.titleMedium),
-              //       ),
-              //       const SizedBox(height: 10.0),
-              //       SizedBox(
-              //         height: 120,
-              //         child: GridView.builder(
-              //           physics: const NeverScrollableScrollPhysics(),
-              //           gridDelegate:
-              //               const SliverGridDelegateWithFixedCrossAxisCount(
-              //             crossAxisCount: 4,
-              //             crossAxisSpacing: 15.0,
-              //           ),
-              //           itemCount: 4,
-              //           itemBuilder: (context, index) {
-              //             return CategoryWidget(
-              //               image: categoryImages[index],
-              //               title: categoryTitles[index],
-              //               textTheme: textTheme,
-              //               cardColor: cardColor,
-              //               primaryColor: primaryColor,
-              //             );
-              //           },
-              //         ),
-              //       ),
-              //     ],
-              //   ),
-              // ),
-              // //
-              // const SizedBox(height: 5.0),
-
-              // Lelel categories
-              BlocBuilder<ChangeIndexCubit, int>(
-                builder: (context, state) {
-                  return SizedBox(
-                    width: double.infinity,
-                    child: SingleChildScrollView(
-                      physics: const BouncingScrollPhysics(),
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: List.generate(
-                          categoryTitles.length,
-                          (index) => LebelButton(
-                            primaryColor: primaryColor,
-                            textTheme: textTheme,
-                            state: state,
-                            index: index,
-                            title: categoryTitles[index],
-                          ),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-              //
-              const SizedBox(height: 12.0),
-              // Title List
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('برترین ها', style: textTheme.titleMedium),
-                    Row(
-                      children: [
-                        Text(
-                          'مشاهده همه',
-                          style: TextStyle(color: primaryColor, fontSize: 16.0),
-                        ),
-                        const SizedBox(width: 3.0),
-                        Icon(
-                          Icons.arrow_forward_ios_rounded,
-                          size: 14.0,
-                          color: primaryColor,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              // Top Prices List
-              BlocBuilder<PricesCubit, PricesState>(
-                buildWhen: (previous, current) {
-                  if (previous.pricesDataStatus == current.pricesDataStatus) {
-                    return false;
-                  }
-                  return true;
-                },
-                builder: (context, state) {
-                  // Loading
-                  if (state.pricesDataStatus is PricesDataLoading) {
-                    return RefreshProgressIndicator(
-                      color: Colors.grey.shade800,
-                    );
-                  }
-                  // Completed
-                  if (state.pricesDataStatus is PricesDataCompleted) {
-                    PricesDataCompleted pricesDataCompleted =
-                        state.pricesDataStatus as PricesDataCompleted;
-                    // models
-                    List priceModels = pricesDataCompleted.modelResults;
-
-                    return Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: RefreshIndicator(
-                          color: Colors.grey.shade800,
-                          child: ListView.builder(
-                            physics: const BouncingScrollPhysics(),
-                            itemCount: priceModels.length,
-                            itemBuilder: (context, index) {
-                              // percentIcon
-                              var percentIcon =
-                                  DecimalRounder.setPercentChangesIcon(
-                                      priceModels[index].percentChange);
-
-                              return PriceItem(
-                                index: index,
-                                priceModels: priceModels,
-                                cardColor: cardColor,
-                                labelTitle: HomeScreen.labelTitle,
-                                primaryColor: primaryColor,
-                                textTheme: textTheme,
-                                percentIcon: percentIcon,
-                              );
-                            },
-                          ),
-                          onRefresh: () async {
-                            await Future.delayed(
-                              const Duration(seconds: 1),
-                            );
-
-                            switch (HomeScreen.labelTitle) {
-                              case 'طلا':
-                                BlocProvider.of<PricesCubit>(context)
-                                    .refreshGoldDataEvent();
-                                break;
-                              case 'سکه':
-                                BlocProvider.of<PricesCubit>(context)
-                                    .refreshCoinDataEvent();
-                                break;
-                              case 'ارز مرجع':
-                                BlocProvider.of<PricesCubit>(context)
-                                    .refreshCurrencyDataEvent();
-                                break;
-                              case 'ارز دیجیتال':
-                                BlocProvider.of<PricesCubit>(context)
-                                    .refreshCryptoDataEvent();
-                                break;
-                              default:
-                            }
-                          },
-                        ),
-                      ),
-                    );
-                  }
-                  // Error
-                  if (state.pricesDataStatus is PricesDataCompleted) {
-                    return const Text('Error');
-                  }
-
-                  return Container();
-                },
-              ),
-            ],
-          );
-        }),
+            ),
+            //
+            const SizedBox(height: 10.0),
+          ],
+        ),
       ),
     );
   }
--- lib/features/feature_home/presentation/widgets/category_widget.dart
+++ lib/features/feature_home/presentation/widgets/category_widget.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:price_online/features/feature_prices/presentation/screens/prices_screen.dart';
 
 class CategoryWidget extends StatelessWidget {
   const CategoryWidget({
@@ -21,31 +22,41 @@ class CategoryWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: cardColor,
-        borderRadius: BorderRadius.circular(25.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.shade100,
-            blurRadius: 10.0,
-          ),
-        ],
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SvgPicture.asset(
-            image,
-            width: 35.0,
-            color: primaryColor,
-          ),
-          const SizedBox(height: 15.0),
-          Text(
-            title,
-            style: textTheme.labelMedium,
-          ),
-        ],
+    return GestureDetector(
+      onTap: () {
+        print(title);
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const PricesScreen(),
+            ));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: cardColor,
+          borderRadius: BorderRadius.circular(20.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade100,
+              blurRadius: 10.0,
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              image,
+              width: 40.0,
+              color: primaryColor,
+            ),
+            const SizedBox(height: 20.0),
+            Text(
+              title,
+              style: textTheme.labelMedium,
+            ),
+          ],
+        ),
       ),
     );
   }
